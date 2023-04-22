358dfe40da3f7d074687d9728d5e58fcfb7e25d2
Lucas <molinag.lucas@gmail.com>
2023-04-21 23:46:23 -0700

    Add IP and PORT constants

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants")
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
@@ -13,22 +14,7 @@ const connect = function () {
   conn.on("connect", () => {
     conn.write("Name: LMG");
 
-  // setInterval(() => {  
-  //     conn.write("Move: up");
-  //   }, 2000);
   });
-
-  // conn.on("connect", () => {
-  //   conn.write("Move: down");
-  // })
-
-  // conn.on("connect", () => {
-  //   conn.write("Move: left");
-  // })
-
-  // conn.on("connect", () => {
-  //   conn.write("Move: right");
-  // })
 
   conn.on("data", (data) => {
     console.log(data.toString())
@@ -40,8 +26,5 @@ const connect = function () {
 
   return conn;
 };
-
-// console.log("Connecting ...");
-// connect();
 
 module.exports = connect;
