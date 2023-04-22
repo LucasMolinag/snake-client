b0a3e22071b33c3f5cc04f50f966e41325417d1b
Lucas <molinag.lucas@gmail.com>
2023-04-21 23:33:04 -0700

    Implement movement - console.log movement information

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,7 +17,19 @@ const setupInput = function () {
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === "w") {
+    console.log("Move: up")
+    connection.write("Move: up");
+  } else if (data === "a") {
+    console.log("Move: left")
+    connection.write("Move: left");
+  } else if (data === "s") {
+    console.log("Move: down")
+    connection.write("Move: down");
+  } else if (data === "d") {
+    console.log("Move: right")
+    connection.write("Move: right");
   }
-};
+}
 
 module.exports = setupInput;
